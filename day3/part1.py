bb11caf965f46417405597690c8117884b312982
Pim van de Streek <git@vandestreek.it>
2023-12-20 00:13:19 +0100

    include day 3 part 1

--- day3/part1.py
+++ day3/part1.py
@@ -1,28 +1,30 @@
-input_path = 'day3/input/example.txt'
+input_path = 'day3/input/input.txt'
 
 file = open(input_path).read().strip()
 lines = file.split('\n')
 
+lines.insert(0, '.' * len(lines[0]))
+lines.append('.' * len(lines[0]))
+lines = ['.' + line + '.' for line in lines]
+
 sum = 0
-digit = []
+digit = ''
 
 for i, line in enumerate(lines):
     for j, c in enumerate(line):
         if c.isdigit():
-            digit.append(c)
+            digit = digit + c
+            continue
 
+        if len(digit) == 0:
+            continue
 
-
-            # if last digit of line
-
-        else:
+        for k in range(i-1, i+2):
+            check_string = lines[k][j - (len(digit)+1):j+1]
+            if len(check_string.strip('.1234567890')) != 0:
+                sum += int(digit)
+                break
         
-            #determine if symbol is near
-        
-            #reset digit
-            digit = []
+        digit = ''
 
-
-
-
-print(lines)
+print(sum)
